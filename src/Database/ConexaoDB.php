ea9374a4d8c7b014260836934467baa90d19bed7
mayconr4 <mayconhs2007@gmail.com>
2025-05-19 09:21:26 -0300

    feito composer e organizacao de algumas classes

---
+++ src/Database/ConexaoDB.php
@@ -0,0 +1,14 @@
+<?php 
+namespace ExplosaoCultural\DataBase;
+
+use PDO;
+
+abstract class ConexaoDB 
+{ 
+    private static PDO $conexao;
+    private static string $servidor = "localhost";
+    private static string $usuario = "root";
+    private static string $senha = "";
+    private static string $banco = 
+
+}
