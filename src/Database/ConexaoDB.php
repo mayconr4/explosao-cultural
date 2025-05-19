03b42913a55e9ce7b51953a1a007433620ae467f
mayconr4 <mayconhs2007@gmail.com>
2025-05-19 09:53:56 -0300

    feito conexao com o bd

--- src/Database/ConexaoDB.php
+++ src/Database/ConexaoDB.php
@@ -1,7 +1,9 @@
 <?php 
 namespace ExplosaoCultural\DataBase;
 
+use Exception;
 use PDO;
+use Throwable;
 
 abstract class ConexaoDB 
 { 
@@ -9,6 +11,27 @@ abstract class ConexaoDB
     private static string $servidor = "localhost";
     private static string $usuario = "root";
     private static string $senha = "";
-    private static string $banco = 
+    private static string $banco = "vendas";
+
+    public static function getConexao():PDO 
+    { 
+        if(!isset(self::$conexao)){ 
+
+            try{ 
+                self::$conexao = new PDO( 
+                    "mysql:host=".self::$servidor.";dbname=".self::$banco.";charset=utf8",
+                    self::$usuario, self::$senha
+                ); 
+
+
+                self::$conexao->setAttribute (PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
+            }   catch (Throwable $erro){ 
+
+
+                throw new Exception("erro ao conectar com o banco de dados !").$erro->getMessage();
+            }
+        }
+        return self::$conexao;
+    }
 
 }
---
+++ src/Models/Eventos.php
@@ -0,0 +1,11 @@
+<?php 
+namespace ExplosaoCultural\Models; 
+
+
+final final class Eventos 
+{
+    private ?int $id; 
+    private string $nome;
+    private string $horario;
+    private string 
+}
