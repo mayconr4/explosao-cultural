134b0100f4893bcef2aa845eafa027b0529e260b
mayconr4 <mayconhs2007@gmail.com>
2025-05-20 10:35:43 -0300

    tirar o data

--- src/Models/Usuarios.php
+++ src/Models/Usuarios.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 
 namespace ExplosaoCultural\Models;
+
 use ExplosaoCultural\Enums\TipoGenero;
 use ExplosaoCultural\Enums\TipoUsuario;
 
-final class Usuarios 
-{ 
+final class Usuarios
+{
     private ?int $id;
     private string $nome;
     private string $dataNascimento;
     private string $email;
     private string $senha;
-    private TipoUsuario $tipoDoUser; 
+    private TipoUsuario $tipoDoUser;
 
 
 
@@ -28,70 +29,69 @@ final class Usuarios
         $this->setEmail($email);
         $this->setSenha($senha);
         $this->setTipoDoUser($tipoDoUser);
-        $this->setID($id); 
-    } 
+        $this->setID($id);
+    }
 
     //getters 
-    public function getId(): ?int 
-    { 
+    public function getTipoDoUser(): TipoUsuario
+    {
+        return $this->tipoDoUser;
+    }
+    public function setTipoDoUser(TipoUsuario $tipoDoUser): void
+    {
+        $this->tipoDoUser = $tipoDoUser;
+    }
+
+    public function getId(): ?int
+    {
         return $this->id;
     }
 
     public function getNome(): string
-    { 
+    {
         return $this->nome;
-    } 
+    }
 
     public function getDataNascimento(): string
-    { 
+    {
         return $this->dataNascimento;
     }
-    
-    
+
+
     public function getEmail(): string
-    { 
+    {
         return $this->email;
     }
 
     public function getSenha(): string
-    { 
+    {
         return $this->senha;
     }
 
-    public function getTipoDoUser(): TipoUsuario
-    { 
-        return $this->tipoDoUser;
-    } 
 
     //setters 
     public function setId(?int $id): void
-    { 
+    {
         $this->id = $id;
     }
 
     public function setNome(string $nome): void
-    { 
+    {
         $this->nome = $nome;
     }
 
     public function setDataNascimento(string $dataNascimento): void
-    { 
+    {
         $this->dataNascimento = $dataNascimento;
     }
 
     public function setEmail(string $email): void
-    { 
+    {
         $this->email = $email;
     }
 
     public function setSenha(string $senha): void
-    { 
+    {
         $this->senha = $senha;
     }
-
-    public function setTipoDoUser(TipoUsuario $tipoDoUser): void
-    { 
-        $this->tipoDoUser = $tipoDoUser;
-    }
-
 }
