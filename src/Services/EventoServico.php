ebee5b0a7526ec04791587598defe6364d3f97cd
mayconr4 <mayconhs2007@gmail.com>
2025-05-20 08:59:45 -0300

    s

--- src/Services/EventoServico.php
+++ src/Services/EventoServico.php
@@ -35,10 +35,10 @@ final class EventoServico
         usuarios.nome AS criador,
         generos.nome AS genero,
         CONCAT(enderecos.rua, ', ', enderecos.numero, ' - ', enderecos.cidade) AS endereco
-    FROM eventos
-    JOIN usuarios ON eventos.usuario_id = usuarios.id
-    JOIN generos ON eventos.genero_id = generos.id
-    JOIN enderecos ON eventos.endereco_id = enderecos.id         
+        FROM eventos
+        JOIN usuarios ON eventos.usuario_id = usuarios.id
+        JOIN generos ON eventos.genero_id = generos.id
+        JOIN enderecos ON eventos.endereco_id = enderecos.id         
         ";
     // WHERE eventos.status = 1 
     // ORDER BY eventos.datas DESC, eventos.horario DESC Olhar com calma está relacionado a o metofo que você tem que desenvolver para o evento mais proximos terem preferenci a de exibição
