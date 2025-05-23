1042e7f14170a2c9b2f4be3a0593ebde7b790dda
mayconr4 <mayconhs2007@gmail.com>
2025-05-23 08:34:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $listaDeEventos = $eventoServico->listarTodos();
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Explosãosssss Cultural</title>
+  <title>Explosãosj Cultural</title>
   <link rel="stylesheet" href="css/estilo.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
